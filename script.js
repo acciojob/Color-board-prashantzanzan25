df6a9497f49c8e8283791a0bb85b49d6acbfc5bb
Prashant Zanzan <139844406+prashantzanzan25@users.noreply.github.com>
2023-12-14 11:58:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+const container = document.getElementById('colorBoard');
+
+  for (let i = 0; i < 300; i++) {
+    const square = document.createElement('div');
+    square.classList.add('square');
+    container.appendChild(square);
+  }
