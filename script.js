a889ad251fbef3c29f53f585179b771d2214cbc0
Prashant Zanzan <139844406+prashantzanzan25@users.noreply.github.com>
2024-02-15 13:56:48 +0530

    created a submission

--- script.js
+++
@@ -1,7 +0,0 @@
-const board = document.getElementById('board');
-
-for (let i = 0; i < 800; i++) {
-  const square = document.createElement('div');
-  square.classList.add('square');
-  board.appendChild(square);
-}
